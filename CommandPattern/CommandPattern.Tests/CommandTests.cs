8ca37ae04b540dc3a4fb9570534473b36aae882a
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2019-10-09 07:45:24 +0200

    More unit tests added.

--- CommandPattern/CommandPattern.Tests/CommandTests.cs
+++ CommandPattern/CommandPattern.Tests/CommandTests.cs
@@ -3,6 +3,7 @@ using CommandPattern.DataAccess.DataSources;
 using CommandPattern.Domain.Commands;
 using CommandPattern.Domain.Models;
 using FluentAssertions;
+using NSubstitute;
 using Xunit;
 
 namespace CommandPattern.Tests
@@ -23,6 +24,37 @@ namespace CommandPattern.Tests
             var containSingle = DataSource.Cars.Should().ContainSingle(x => x.RegNo == "RNY293").Subject;
             containSingle.InspectedAt.Should().Be(inspectedAt);
             containSingle.InspectionApproved.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CarInspectionFailed_ShouldInvokeDataSource()
+        {
+            var dataSource = Substitute.For<InMemoryDataSource>();
+
+            var domainModel = new Car("RNY293");
+            var inspectedAt = DateTimeOffset.UtcNow;
+            var sut = new CarInspectionFailed(inspectedAt);
+
+            sut.Execute(domainModel, dataSource);
+
+            dataSource.Received().Store(Arg.Is<DataAccess.Entities.Car>(car =>
+                car.InspectionApproved == false &&
+                car.RegNo == "RNY293" &&
+                car.InspectedAt == inspectedAt));
+        }
+
+        [Fact]
+        public void CarInspectionFailed_ShouldApplyDomainModelChanges()
+        {
+            var dataSource = Substitute.For<InMemoryDataSource>();
+            var domainModel = Substitute.For<Car>("RNY293");
+
+            var inspectedAt = DateTimeOffset.UtcNow;
+            var sut = new CarInspectionFailed(inspectedAt);
+
+            sut.Execute(domainModel, dataSource);
+
+            domainModel.Received().ApplyInspectionFailed(inspectedAt);
         }
 
         [Fact]
